1539a39d4e7b881ba4e411dcfe84b32d0ee327ef
Jaffar Aman <amanjaffar50@gmail.com>
2021-12-29 13:08:22 +0500

    implement redux

--- src/components/UIComponents/Navbar/Navbar.js
+++ src/components/UIComponents/Navbar/Navbar.js
@@ -7,22 +7,22 @@ const Navbar = () => {
     const history = useHistory();
     return (
         // <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#" onClick={() => history.push("/home")}><img src={logo} width="100" alt="" srcset="" /></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#" onClick={() => history.push("/home")}><img src={logo} width="100" alt="" srcSet="" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item" onClick={() => history.push("/home")}>
-                            <a class="nav-link active" aria-current="page" href="#" onClick={() => history.push("/home")}>Home</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item" onClick={() => history.push("/home")}>
+                            <a className="nav-link active" aria-current="page" href="#" onClick={() => history.push("/home")}>Home</a>
                         </li>
-                        <li class="nav-item" onClick={() => history.push("/signup")}>
-                            <a class="nav-link" href="#">Signup</a>
+                        <li className="nav-item" onClick={() => history.push("/signup")}>
+                            <a className="nav-link" href="#">Signup</a>
                         </li>
-                        <li class="nav-item" onClick={() => history.push("/")}>
-                            <a class="nav-link" href="#">Login</a>
+                        <li className="nav-item" onClick={() => history.push("/")}>
+                            <a className="nav-link" href="#">Login</a>
                         </li>
                     </ul>
                 </div>
